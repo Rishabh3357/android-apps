708dd3cf4000102680d6fd14f77dc555ef3c5d42
Rishabh Saraswat <rishabhsaraswat3357@gmail.com>
2025-10-05 20:14:32 +0530

    Add files via upload

---
+++ mi_card_flutter/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.red,
+                ),
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.blue,
+                  child: Text('Container 2'),
+                ),
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.red,
+                  child: Text('Container 3'),
+                )
+              ],
+            )
+        ),
+      ),
+    );
+  }
+}
